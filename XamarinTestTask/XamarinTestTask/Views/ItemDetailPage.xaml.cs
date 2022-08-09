6a0a171894810d01bc5727e6fe6389e7b3548f4f
Dauchara <pro100_gold_man@mail.ru>
2022-08-09 20:21:35 +0600

    Добавьте файлы проекта.

---
+++ XamarinTestTask/XamarinTestTask/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinTestTask.ViewModels;
+
+namespace XamarinTestTask.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ XamarinTestTask/XamarinTestTask/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinTestTask.Models;
+using XamarinTestTask.ViewModels;
+
+namespace XamarinTestTask.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
