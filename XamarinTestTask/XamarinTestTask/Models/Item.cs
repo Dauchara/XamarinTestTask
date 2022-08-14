698756e8bb52923eb02b98282d66f98d84e3bd12
Dauchara <pro100_gold_man@mail.ru>
2022-08-14 19:33:15 +0600

    tset

--- XamarinTestTask/XamarinTestTask/Models/Item.cs
+++ XamarinTestTask/XamarinTestTask/Models/Item.cs
@@ -7,5 +7,6 @@ namespace XamarinTestTask.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public string Test { get; set; }
     }
 }
